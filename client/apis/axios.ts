fe959b38488fcecf7f315b60c778cf48ca46fb4f
xiaobo <2078350663@qq.com>
2021-09-05 10:07:56 +0800

    Added git hooks, eslint, prettier and rubocop

--- client/apis/axios.ts
+++ client/apis/axios.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
-import Toastr from "../bundles/common/Toastr";
 import { useHistory } from "react-router-dom";
-import { getFromLocalStorage, setToLocalStorage } from "../bundles/helpers/storage";
 
+import Toastr from "../bundles/common/Toastr";
+import {
+  getFromLocalStorage,
+  setToLocalStorage
+} from "../bundles/helpers/storage";
 
 const handleSuccessResponse = response => {
   if (response) {
@@ -20,22 +23,22 @@ const handleErrorResponse = error => {
     setToLocalStorage(null);
   }
   if (error.response?.status === 403) {
-    localStorage.removeItem('token');
-    setToLocalStorage(null)
-    Toastr.error("登录过期，请重新登录")
+    localStorage.removeItem("token");
+    setToLocalStorage(null);
+    Toastr.error("登录过期，请重新登录");
     setTimeout(() => {
-      history.push("/")
+      history.push("/");
     }, 1000);
   }
   Toastr.error(
     error.response?.data?.error ||
-    error.response?.data?.notice ||
-    error.message ||
-    error.notice ||
-    "Something went wrong!"
+      error.response?.data?.notice ||
+      error.message ||
+      error.notice ||
+      "Something went wrong!"
   );
   if (error.response?.status === 423) {
-    history.push("/")
+    history.push("/");
   }
   return Promise.reject(error);
 };
@@ -47,26 +50,27 @@ api.defaults.headers = {
   "Content-Type": "application/json",
   "X-CSRF-TOKEN": document
     .querySelector('[name="csrf-token"]')
-    .getAttribute("content"),
+    .getAttribute("content")
 };
 // 设置post请求头
 // api.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-/** 
- * 请求拦截器 
- * 每次请求前，如果存在token则在请求头中携带token 
+/**
+ * 请求拦截器
+ * 每次请求前，如果存在token则在请求头中携带token
  */
 api.interceptors.request.use(
   config => {
-    // 登录流程控制中，根据本地是否存在token判断用户的登录情况        
-    // 但是即使token存在，也有可能token是过期的，所以在每次的请求头中携带token        
-    // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态码        
-    // 而后我们可以在响应拦截器中，根据状态码进行一些统一的操作。        
+    // 登录流程控制中，根据本地是否存在token判断用户的登录情况
+    // 但是即使token存在，也有可能token是过期的，所以在每次的请求头中携带token
+    // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态码
+    // 而后我们可以在响应拦截器中，根据状态码进行一些统一的操作。
     const token = getFromLocalStorage("token");
-    console.log(token, '333')
+    console.log(token, "333");
     token && (config.headers.Authorization = token);
     return config;
   },
-  error => Promise.reject(error))
+  error => Promise.reject(error)
+);
 
 // 响应拦截器
 axios.interceptors.response.use(handleSuccessResponse, error =>
@@ -74,4 +78,3 @@ axios.interceptors.response.use(handleSuccessResponse, error =>
 );
 
 export default api;
-
--- client/packs/application.js
+++ client/packs/application.js
@@ -13,4 +13,3 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
